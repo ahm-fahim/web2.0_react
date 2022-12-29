09b193acb6952ff0521b177890f0b0a200dabfe2
mdFahimMorshed <contactahmfahim@gmail.com>
2022-12-30 00:53:24 +0600

    react todo

--- 03_react_modules_and_data_store/src/component/TodoList/TodoList.js
+++ 03_react_modules_and_data_store/src/component/TodoList/TodoList.js
@@ -1,27 +1,27 @@
 import React, { useState } from "react";
 import "./TodoList.css";
 
-const TodoList = (key) => {
-    const [input, setInput] = useState(" ");
-    const [todo, setTodo] = useState(" ");
+const TodoList = (key, value) => {
+    const [input, setInput] = useState([]);
+    const [todo, setTodo] = useState([]);
 
     const getValue = (e) => {
         setInput(e.target.value);
     };
 
     const addTodo = () => {
-        console.log(key,input);
         setTodo(input);
-        const newTodo = [...todo]
-        localStorage.setItem(key , newTodo)
+        const newTodo = todo;
+        localStorage.setItem("todo-item", JSON.stringify(newTodo));
     };
+
     return (
         <div className="background">
             <h3>TODO LIST</h3>
             <div>
                 {/* todo list */}
                 <ol>
-                    <li>{todo}</li>
+                    <li>{localStorage.getItem("todo-item")}</li>
                 </ol>
                 <input onChange={getValue} className="inputField" type="text" />
                 <button onClick={addTodo} className="btn">
