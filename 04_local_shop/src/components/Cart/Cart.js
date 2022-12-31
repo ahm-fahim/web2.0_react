a0bddb85f6b853ad99804a0d5e39a855790cf9c9
mdFahimMorshed <contactahmfahim@gmail.com>
2023-01-01 01:34:01 +0600

    add cart

--- 04_local_shop/src/components/Cart/Cart.js
+++ 04_local_shop/src/components/Cart/Cart.js
@@ -2,14 +2,23 @@ import React from "react";
 import "./Cart.css";
 
 const Cart = ({ cart }) => {
-    console.log("cart:",cart);
+    console.log("cart:", cart);
+    let total = 0;
+    let shipping = 0;
+
+    for (const products of cart) {
+        total = total + products.price;
+        shipping = shipping + products.shipping;
+    }
+    const vat = parseFloat((total * 0.1).toFixed(2));
+    const totalAmount = total + shipping + vat;
     return (
         <div className="text-light">
             <p> Total Items : <span className="text-danger fw-bold">{cart.length}</span> </p>
-            <p> Total Price : <span className="text-danger fw-bold"></span> </p>
-            <p> Total Shipping : <span className="text-danger fw-bold"></span>  </p>
-            <p> Vat: <span className="text-danger fw-bold"></span>  </p>
-            <p> Total Amount: <span className="text-warning fw-bold"></span> </p>
+            <p> Total Price : <span className="text-danger fw-bold">{total.toFixed(2)}</span> </p>
+            <p> Total Shipping : <span className="text-danger fw-bold">{shipping.toFixed(2)}</span>  </p>
+            <p> Vat: <span className="text-danger fw-bold">{ vat}</span>  </p>
+            <p> Total Amount: <span className="text-warning fw-bold">{totalAmount.toFixed(2)}</span> </p>
         </div>
     );
 };
